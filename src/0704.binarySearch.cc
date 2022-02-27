4be8700499f68a7130aedffb5009ca2fc851adb3
alfred_gorski <dzhongzhu@icloud.com>
2022-02-27 22:02:30 +0100

    704. Binary Search

---
+++ src/0704.binarySearch.cc
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <array>
+#include <vector>
+
+#include "catch2/catch.hpp"
+
+namespace std {
+
+class Solution {
+ public:
+  int search(vector<int>& nums, int target) {
+    auto it = lower_bound(nums.begin(), nums.end(), target);
+    return (it != nums.end() && *it == target) ? distance(nums.begin(), it)
+                                               : -1;
+  }
+};
+
+TEST_CASE("704. Binary Search") {
+  Solution s;
+  vector<int> nums = {-1, 0, 3, 5, 9, 12};
+  int target = 9;
+  int output = 4;
+  REQUIRE(s.search(nums, target) == output);
+  nums = {-1, 0, 3, 5, 9, 12};
+  target = 2;
+  output = -1;
+  REQUIRE(s.search(nums, target) == output);
+  nums = {-1};
+  target = 2;
+  output = -1;
+  REQUIRE(s.search(nums, target) == output);
+}
+}  // namespace std

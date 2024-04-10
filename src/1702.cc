77681132ea24c73ce057207e0360a7f0b1292a91
Zhongzhu Deng <dzhongzhu@icloud.com>
2024-04-10 16:14:34 +0200

    1702. Maximum Binary String After Change

---
+++ src/1702.cc
@@ -0,0 +1,28 @@
+#include <catch2/catch_test_macros.hpp>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+  string maximumBinaryString(string binary) {
+    size_t n = binary.size(), i = binary.find('0');
+    if (i == std::string::npos) 
+      return binary;
+
+    size_t zeros = count(binary.begin(), binary.end(), '0');
+    std::string s(n, '1');
+    s[i + zeros - 1] = '0';
+    return s;
+  }
+};
+
+TEST_CASE("1702. Maximum Binary String After Change", "[1702]") {
+  Solution s;
+  std::string in = "000110";
+  std::string ans = "111011";
+  REQUIRE(s.maximumBinaryString(in) == ans);
+
+  in = "01";
+  ans = "01";
+  REQUIRE(s.maximumBinaryString(in) == ans);
+}
